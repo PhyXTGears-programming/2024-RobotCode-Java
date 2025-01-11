976544fea0b4bacfa51121f52ef9be27e341336b
PhyXTGears <phyxtgearspro@gmail.com>
2025-01-11 14:51:07 -0500

    Add drivetrain constants
    - Justin C

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,5 +1,8 @@
 package frc.robot;
 
 public class Constants {
-    
+    public class Drive {
+        // FIXME: set this to real value
+        public static final double kTrunWheelPerMotorRatio = 0.0;
+    }
 }
