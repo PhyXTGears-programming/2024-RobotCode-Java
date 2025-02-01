f1f7de4d5ff6b16a6759afefb1596d23e0e68dfd
Joshua B <joshuabutler312@gmail.com>
2025-02-01 18:17:03 -0500

    Add some missing function methods
    -Joshua B

--- src/main/java/frc/robot/auto/Auto.java
+++ src/main/java/frc/robot/auto/Auto.java
@@ -1,6 +1,12 @@
 package frc.robot.auto;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -9,11 +15,14 @@ import edu.wpi.first.units.measure.LinearVelocity;
 import edu.wpi.first.units.Measure;
 import edu.wpi.first.units.Units;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
+import frc.robot.auto.PathPoint.PathPointType;
 import frc.robot.auto.load.SubsystemRegistry;
 import frc.robot.math.Vector;
 import frc.robot.subsystems.drive.Drivetrain;
+import frc.robot.commands.FollowPath;
 
 public class Auto {
     public static ArrayList<Pose2d> loadPosePathFromJSON(JSONArray json) {
